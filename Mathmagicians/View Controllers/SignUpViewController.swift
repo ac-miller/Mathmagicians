3de2aab50f42f90d670d01bbe409e36c5b4c9d32
Aaron Miller <milleaar@oregonstate.edu>
2019-10-19 17:09:52 -0500

    Edited View Controllers for Sign Up and Login: 1. Changed segues to Welcome screen to be manually called upon success from Firebase 2. Changed error label to dynamically appear. Might want this to be an alert instead later 3. Targeted the current module for each UIViewController, otherwise causes app to crash 4. Changed password fields to be secure entry

--- Mathmagicians/View Controllers/SignUpViewController.swift
+++ Mathmagicians/View Controllers/SignUpViewController.swift
@@ -7,15 +7,55 @@
 //
 
 import UIKit
+import Firebase
 
 class SignUpViewController: UIViewController {
 
+    //not using first/last name yet. Must save those
+    //by using the return userID from auth.createuser
+    //we can store them in a subdatabase
+    
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var errorLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        errorLabel.isHidden = true
+        errorLabel.numberOfLines = 0
         
     }
     
+    @IBAction func signupPressed(_ sender: UIButton) {
+        
+        Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) {
+            (user, error) in
+            if error == nil {
+                print("Registration Successful")
+                self.performSegue(withIdentifier: "signedSuccess", sender: nil)
+            } else {
+                let errorCode = AuthErrorCode(rawValue: error!._code)
+                
+                switch errorCode {
+                case .emailAlreadyInUse?:
+                    self.errorLabel.text = "ERROR: Email Already in Use"
+                case .invalidEmail?:
+                    self.errorLabel.text = "ERROR: Email is incorrect format"
+                case .wrongPassword?:
+                    self.errorLabel.text = "ERROR: Wrong Password"
+                case .networkError?:
+                    self.errorLabel.text = "ERROR: Network Error"
+                case .weakPassword?:
+                    self.errorLabel.text = "ERROR: Password must be at least 6 characters"
+                default:
+                    self.errorLabel.text = "ERROR: Try Again Later"
+                }
+                self.errorLabel.isHidden = false
+                
+            }
+        }
+    }
     
 }
