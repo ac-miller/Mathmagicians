582abb21c7849e75965c36bb48a795db2d29c916
jesse <chanjess@oregonstate.edu>
2019-10-31 17:37:04 -0700

    adding monsters to map

--- Mathmagicians/View Controllers/MapExploreController.swift
+++ Mathmagicians/View Controllers/MapExploreController.swift
@@ -10,7 +10,7 @@ import UIKit
 import MapKit
 
 //locationManagerDelegate takes care of all of user location info
-class MapExploreController: UIViewController, CLLocationManagerDelegate {
+class MapExploreController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     
@@ -39,12 +39,31 @@ class MapExploreController: UIViewController, CLLocationManagerDelegate {
         //setting up location authorization status
         //if the authorization status has been set, then we are clear to show user location on the map
         if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            
+            self.mapView.delegate = self
+            
             self.mapView.showsUserLocation = true
             
             //to update user location as they move
             self.manager.startUpdatingLocation()
             
             
+            //randomly placing annotation on the map (annotation represents monsters)
+            Timer.scheduledTimer(withTimeInterval: 5, repeats: true, block: { (timer) in
+                
+                if let coordinate = self.manager.location?.coordinate {
+                    
+                    let annotation = MKPointAnnotation()
+                    
+                    annotation.coordinate = coordinate
+                    
+                    annotation.coordinate.latitude += (Double(arc4random_uniform(1000)) - 500) / 300000.0
+                    annotation.coordinate.longitude += (Double(arc4random_uniform(1000)) - 500) / 300000.0
+                    
+                    self.mapView.addAnnotation(annotation)
+                }
+            
+            })
             //if we do not have authorization, then request access from user
         } else {
             //only asks the user for location request when the app is in use
@@ -53,6 +72,34 @@ class MapExploreController: UIViewController, CLLocationManagerDelegate {
         
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: nil)
+        
+        //random number used to randomize spawning of different monsters
+        let number = Int.random(in: 1 ..< 4)
+        
+        if annotation is MKUserLocation {
+                
+            annotationView.image = UIImage(named: "wizard")
+        } else if number == 1 {
+            annotationView.image = UIImage(named: "easyGreenMonster")
+        } else if number == 2 {
+            annotationView.image = UIImage(named: "mediumBlueMonster")
+        } else {
+            annotationView.image = UIImage(named: "hardRedMonster")
+        }
+        
+        var newFrame = annotationView.frame
+        newFrame.size.height = 40
+        newFrame.size.width = 40
+        annotationView.frame = newFrame
+    
+        return annotationView
+    }
+    
+    
     
     //
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
