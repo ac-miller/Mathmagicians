c81a03a04a06930e3e52cf14980d84a42132e77d
jesse <chanjess@oregonstate.edu>
2019-11-08 17:52:14 -0800

    added timer and time out message to lead back to map page

--- Mathmagicians/View Controllers/encounterController.swift
+++ Mathmagicians/View Controllers/encounterController.swift
@@ -14,6 +14,27 @@ class encounterController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet weak var sceneView: ARSCNView!
     
+    @IBOutlet weak var countdownLbl: UILabel!
+    
+    
+    //for timer functionality
+    var countDown = 15
+    var timer = Timer()
+    
+    
+    //for alert code when user runs out of time
+    func createAlert (title: String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (action) in self.performSegue(withIdentifier:
+            "backToMap", sender: self)
+            
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         
         
@@ -27,8 +48,31 @@ class encounterController: UIViewController, ARSCNViewDelegate {
 
         sceneView.scene = scene
         
+        //start timer
+        startCountDown()
+        
         addBeastie()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //func to initialize timer
+    func startCountDown(){
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(countDownMethod), userInfo: nil, repeats: true)
+    }
+    
+    @objc func countDownMethod(){
+        countDown -= 1
+        countdownLbl.text = "\(countDown)"
+        
+        //stop timer when it reaches 0 seconds
+        if (countDown == 0)
+        {
+            timer.invalidate()
+            createAlert(title: "MATHMAGICIAN, YOU RAN OUT OF TIME!", message: "We're taking you back to the map")
+            
+            performSegue(withIdentifier: "backToMap", sender: nil)
+        }
     }
     
     func addBeastie(){
