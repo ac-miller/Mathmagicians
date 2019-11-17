2e1b45c90a063025b49935c9a8433f7affce083f
Aaron Miller <ac.miller@outlook.com>
2019-11-17 16:50:43 -0600

    Created table view for beasties captured in backpack Created custom cell for table view

---
+++ Mathmagicians/Model/BackpackCell.swift
@@ -0,0 +1,15 @@
+//
+//  BackpackCell.swift
+//  Mathmagicians
+//
+//  Created by Aaron Miller on 11/17/19.
+//  Copyright © 2019 Jesse Chan. All rights reserved.
+//
+
+import Foundation
+
+struct BackpackCell {
+    let beastie: String //asset name
+    let question: String
+    let answer: String
+}
--- Mathmagicians/View Controllers/BackpackInventoryViewController.swift
+++ Mathmagicians/View Controllers/BackpackInventoryViewController.swift
@@ -7,28 +7,41 @@
 //
 
 import UIKit
+import Firebase
 
 class BackpackInventoryViewController: UIViewController {
 
+    var beasties: [BackpackCell] = [
+        BackpackCell(beastie: "dragon", question: "1+1", answer: "2"),
+        BackpackCell(beastie: "cat", question: "2+2", answer: "4"),
+        BackpackCell(beastie: "scary", question:"4+4", answer: "8")
+    ]
+    
+    @IBOutlet var beastieTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("loaded backpack")
-        // Do any additional setup after loading the view.
+        
+        beastieTable.dataSource = self
+        beastieTable.register(UINib(nibName: "BeastieBackpackCell", bundle: nil), forCellReuseIdentifier: "BeastieCell")
 
     }
     
     //need to get beasties caught from firebase or core data
-    //then decode into Beastie object
-    //then display in inventory
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension BackpackInventoryViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return beasties.count
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = beastieTable.dequeueReusableCell(withIdentifier: "BeastieCell", for: indexPath) as! BeastieBackpackCell
+        cell.beastieName.text = beasties[indexPath.row].beastie
+        cell.questionLabel.text = beasties[indexPath.row].question
+        cell.answerLabel.text = beasties[indexPath.row].answer
+        return cell
+    }
+    
 }
